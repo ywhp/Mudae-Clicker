2c2fca0a8270526a43e1a8590b4eaebd9be0297a
JaromirLipsky <100523022+JaromirLipsky@users.noreply.github.com>
2024-11-26 13:44:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,21 +4,30 @@ const { Client,WebhookClient, MessageEmbed} = require('discord.js-selfbot-v13');
    console.log(`${client.user.username} is Ready!`);
  })
 const {token} = require('./config.json')
-const likelist = require("./likelist");
+const WishList = require("./WishList");
 
-function MudaeClicker (guildId,channelId,Webhook){
+
+function AutoClicker (guildId,channelId,Webhook){
 const Hook = new WebhookClient({ url: `${Webhook}`});
-client.on("messageCreate",message=> {
-  for (let i=0;i<likelist.length;+i++){
-    let Object = likelist[i]
-  message.embeds.filter(embed => {
+let lastClaim = null;
+let claimTime = null;
+client.on("messageCreate", async message=> {
+  for (let i=0;i<wishList.length;+i++){
+  let Object = wishList[i]
+  message.embeds.filter(async embed => {
     if(message.guildId !=guildId)return;
-    if(!Webhook)return;
     if(message.channelId === channelId){
     if(embed.footer?.text.startsWith("Belongs to"))return;
+    if(embed.footer?.text.includes('/'))return;
     if(embed.author){
       if(embed.author.name === Object){
-      message.clickButton();
+        let currentTime = new Date();
+        if(claimTime && lastClaim && currentTime - lastClaim < 60 * 60 * 100){
+          console.log('claim timer active , skipping...')
+          return;
+        }
+          
+      await message.clickButton();
       let embed = new MessageEmbed()
       .setTitle(`${Object}`)
       .setColor("#fbe7a1")
@@ -27,13 +36,22 @@ client.on("messageCreate",message=> {
       Hook.send({
         embeds: [embed]
    })
+   lastClaim = new Date();
+   let currentMinutes = lastClaim.getMinutes()
+   let remainingMinutes = 60 - currentMinutes;
+   let resetTime = remainingMinutes * 60 * 1000;
+   claimTime = setTimeout(() => {
+     claimTime = null;
+     lastClaim = null;
+     console.log('claim reset..')
+   },resetTime);
     }
   }
   }
-  })
+    })
 }
 })
 }
-MudaeClicker("guildId","channelId","WebHookURL")
+AutoClicker("guildId","channelId","WebHookURL")
 
 client.login(token);
